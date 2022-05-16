768fc8346cab4f8eb55696d498e1c6d944484e3f
Domenico Francesco Giacobbi <domenico.giacobbi@studio.unibo.it>
2022-05-17 00:40:50 +0200

    add default test of gradle

---
+++ src/test/java/puttingchallenge/AppTest.java
@@ -0,0 +1,17 @@
+package puttingchallenge;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * 
+ */
+class AppTest {
+    /**
+     * 
+     */
+    @Test void testApp() {
+        assertTrue(Boolean.TRUE);
+    }
+}
