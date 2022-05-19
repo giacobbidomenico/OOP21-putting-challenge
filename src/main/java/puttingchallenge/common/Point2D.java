1f517fb5fd6faa2f5fbae5639ff9b768528040c9
GiorgioFantilli <giorgio.fantilli@studio.unibo.it>
2022-05-19 16:33:10 +0200

    doc: added documentation to Point2D

--- src/main/java/puttingchallenge/common/Point2D.java
+++ src/main/java/puttingchallenge/common/Point2D.java
@@ -1,9 +1,10 @@
 package puttingchallenge.common;
 
 import java.io.Serializable;
+
 /**
  * 
- * 
+ * 2-dimensional point.
  *
  */
 public class Point2D implements Serializable {
@@ -11,31 +12,32 @@ public class Point2D implements Serializable {
     private static final long serialVersionUID = -6448133475513092884L;
     private final double x, y;
     /**
+     * Build a new {@link Point2D}.
      * 
      * @param x
+     *          abscissa of the 2D point
      * @param y
+     *          ordinate of the 2D point
      */
     public Point2D(final double x, final double y) {
         this.x = x;
         this.y = y;
     }
     /**
-     * 
+     * {@inheritDoc}
      */
     @Override
     public String toString() {
         return "Point2D (" + x + ", " + y + ")";
     }
     /**
-     * 
-     * @return x
+     * @return abscissa of the 2D point
      */
     public double getX() {
         return x;
     }
     /**
-     * 
-     * @return y
+     * @return ordinate of 2D point
      */
     public double getY() {
         return y;
