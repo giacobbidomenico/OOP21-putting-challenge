9a2fd3742fbf4be2b238647f35fdc8f0ba5692ff
Domenico Francesco Giacobbi <domenico.giacobbi@studio.unibo.it>
2022-05-20 17:28:04 +0200

    create GameObject interface

---
+++ src/main/java/puttingchallenge/model/GameObject.java
@@ -0,0 +1,37 @@
+package puttingchallenge.model;
+import puttingchallenge.common.Point2D;
+import puttingchallenge.common.Vector2D;
+
+/**
+ * Class that implements an element of the game.
+ */
+public interface GameObject {
+    /**
+     * Sets the coordinates corresponding to the position of the element.
+     * @param x
+     *          abscissa of the position
+     * @param y
+     *          ordinate of the position
+     */
+    void setPosition(double x, double y);
+    /**
+     * Sets the vector components.
+     * @param x
+     *          x-component of the vector
+     * @param y
+     *          y-component of the vector
+     */
+    void setVector(double x, double y);
+    /**
+     * @return the type of the element
+     */
+    GameObjectType getType();
+    /**
+     * @return the coordinates of the element
+     */
+    Point2D getPosition();
+    /**
+     * @return the components of the vector
+     */
+    Vector2D getVector();
+}
