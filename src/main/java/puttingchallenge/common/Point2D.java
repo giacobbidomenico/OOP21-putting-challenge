68b7fb1e08661ae4006625775a9b0fea1eb07536
GiorgioFantilli <giorgio.fantilli@studio.unibo.it>
2022-05-24 19:21:38 +0200

    feat: implement Point2D equals and hashCode methods

--- src/main/java/puttingchallenge/common/Point2D.java
+++ src/main/java/puttingchallenge/common/Point2D.java
@@ -2,6 +2,8 @@ package puttingchallenge.common;
 
 import java.io.Serializable;
 
+import it.unibo.oop.lab05.ex5.Pair;
+
 /**
  * 
  * 2-dimensional point.
@@ -11,6 +13,7 @@ public class Point2D implements Serializable {
 
     private static final long serialVersionUID = -6448133475513092884L;
     private final double x, y;
+
     /**
      * Build a new {@link Point2D}.
      * 
@@ -23,6 +26,7 @@ public class Point2D implements Serializable {
         this.x = x;
         this.y = y;
     }
+
     /**
      * {@inheritDoc}
      */
@@ -30,17 +34,49 @@ public class Point2D implements Serializable {
     public String toString() {
         return "Point2D (" + x + ", " + y + ")";
     }
+
     /**
      * @return abscissa of the 2D point
      */
     public double getX() {
         return x;
     }
+
     /**
      * @return ordinate of 2D point
      */
     public double getY() {
         return y;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int hashCode() {
+        return Double.hashCode(x) ^ Double.hashCode(y);
+    }
+
+    /**
+     * Compares this point to the specified object. The result is true if and
+     * only if the argument is not null and is a instance of {@link Point2D} and 
+     * contains the same coordinates.
+     * 
+     * @param obj
+     *          the object to compare
+     * 
+     * @return true if the given object is equal to this point, false otherwise
+     */
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj instanceof Point2D) {
+            final var p = (Point2D) obj;
+            return this.x == p.x && this.y == p.y;
+        }
+        return false;
     }
 
 }
