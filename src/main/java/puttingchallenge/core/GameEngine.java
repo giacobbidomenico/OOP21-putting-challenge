a02579c05bdfa8bd63a9fbe2dc3d69d40693e899
GiorgioFantilli <giorgio.fantilli@studio.unibo.it>
2022-06-15 15:41:03 +0200

    feat: set up game engine class

---
+++ src/main/java/puttingchallenge/core/GameEngine.java
@@ -0,0 +1,19 @@
+package puttingchallenge.core;
+
+import puttingchallenge.model.WorldEvent;
+import puttingchallenge.model.WorldEventListener;
+
+/**
+ * Engine of the entire game.
+ */
+public class GameEngine implements WorldEventListener {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void notifyEvent(final WorldEvent e) {
+        // TODO Auto-generated method stub
+    }
+
+}
