f57d7291f9b051cc5b57620542ab25af788bc51f
GiorgioFantilli <giorgio.fantilli@studio.unibo.it>
2022-06-16 21:52:05 +0200

    doc: added javadoc

--- src/main/java/puttingchallenge/graphics/BallGraphicComponent.java
+++ src/main/java/puttingchallenge/graphics/BallGraphicComponent.java
@@ -9,6 +9,12 @@ public final class BallGraphicComponent extends AbstractGraphicComponent {
 
     private static final String SKIN_PATH = "";
 
+    /**
+     * Build a new {@link GraphicComponent} for the game ball.
+     * 
+     * @param gc
+     *          the {@link GraphicsContext} in which the object has to be drawn
+     */
     public BallGraphicComponent(final GraphicsContext gc) {
         super(SKIN_PATH, gc);
     }
