6574667730c8c0d35975b1735eac19825d26be0a
GiorgioFantilli <giorgio.fantilli@studio.unibo.it>
2022-06-16 21:48:34 +0200

    feat: implement graphic component of the ball

---
+++ src/main/java/puttingchallenge/graphics/BallGraphicComponent.java
@@ -0,0 +1,16 @@
+package puttingchallenge.graphics;
+
+import javafx.scene.canvas.GraphicsContext;
+
+/**
+ * Represent the graphic behavior of the game ball.
+ */
+public final class BallGraphicComponent extends AbstractGraphicComponent {
+
+    private static final String SKIN_PATH = "";
+
+    public BallGraphicComponent(final GraphicsContext gc) {
+        super(SKIN_PATH, gc);
+    }
+
+}
