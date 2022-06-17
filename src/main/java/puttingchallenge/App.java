2c3c0cd6bd53ebe94bb0aec2b465f4b659166e85
GiorgioFantilli <giorgio.fantilli@studio.unibo.it>
2022-06-17 15:09:54 +0200

    feat: set up the entry class of the application

--- src/main/java/puttingchallenge/App.java
+++ src/main/java/puttingchallenge/App.java
@@ -1,14 +1,28 @@
 package puttingchallenge;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 /**
- *
+ * The class that start the application.
  */
-public final class App {
-    private App() {
-    }
+public final class App extends Application {
+
     /**
+     * The entry point of the application.
      * 
      * @param args
+     *          unused
      */
     public static void main(final String[] args) {
+        launch(args);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void start(final Stage primaryStage) throws Exception {
+        final GameEngine gameEngine = new GameEngine(primaryStage);
     }
 }
