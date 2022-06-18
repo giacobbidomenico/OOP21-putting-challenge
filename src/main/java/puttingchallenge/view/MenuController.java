6de401f1d7f75fc5e7c5cf16341438142591758c
Domenico Francesco Giacobbi <domenico.giacobbi@studio.unibo.it>
2022-06-18 22:37:16 +0200

    feat: implement the MenuController class

---
+++ src/main/java/puttingchallenge/view/MenuController.java
@@ -0,0 +1,30 @@
+package puttingchallenge.view;
+
+import java.util.List;
+
+import javafx.scene.Scene;
+import puttingchallenge.model.GameObject;
+
+/**
+ * Class that defines the controller that manages the {@link Scene} related to the main menu.
+ * 
+ */
+public class MenuController extends AbstractSceneController {
+
+    /**
+     * Build a new {@link MenuController}.
+     * 
+     * @param scene
+     *            {@link Scene} relating to the main menu
+     * @param gameObjects
+     *            {@link GameObject} present in the {@link Scene}
+     * @param view
+     *            {@link View} where the {@link Scene} is displayed
+     */
+    public MenuController(final Scene scene, 
+                          final List<GameObject> gameObjects, 
+                          final View view) {
+        super(scene, gameObjects, view);
+    }
+
+}
