f58f79da4abcee432c77cd79b816c24b99c3905d
GiorgioFantilli <giorgio.fantilli@studio.unibo.it>
2022-06-19 16:56:05 +0200

    feat: the game engine is a game events observer

--- src/main/java/puttingchallenge/core/GameEngine.java
+++ src/main/java/puttingchallenge/core/GameEngine.java
@@ -1,9 +1,11 @@
 package puttingchallenge.core;
 
+import puttingchallenge.model.WorldEventListener;
+
 /**
  * Represent a basic engine for the game.
  */
-public interface GameEngine {
+public interface GameEngine extends WorldEventListener {
 
     /**
      * Defines a time window within which to process inputs, update internal state and render graphics.
