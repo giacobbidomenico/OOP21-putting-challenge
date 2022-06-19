7db297fd6e2aed477cec65ced255e5de03e78671
Luca Lucioli <luca.lucioli@studio.unibo.it>
2022-06-19 12:57:55 +0200

    feat: add enum for aiming and shooting state

---
+++ src/main/java/puttingchallenge/model/GameStatus.java
@@ -0,0 +1,16 @@
+package puttingchallenge.model;
+/**
+ * 
+ * Enumeration for the various states of the game.
+ *
+ */
+public enum GameStatus {
+    /**
+     * The player is currently aiming.
+     */
+    AIMING,
+    /**
+     * The player finally shoots the ball.
+     */
+    SHOOTING
+}
