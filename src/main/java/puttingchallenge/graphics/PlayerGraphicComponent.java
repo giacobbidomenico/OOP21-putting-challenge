4f8f52bd4b7a159a2fd9b84637f7c06507cdecd6
Luca Lucioli <luca.lucioli@studio.unibo.it>
2022-06-20 13:39:09 +0200

    feat: add show attribute for rendering

--- src/main/java/puttingchallenge/graphics/PlayerGraphicComponent.java
+++ src/main/java/puttingchallenge/graphics/PlayerGraphicComponent.java
@@ -1,10 +1,16 @@
 package puttingchallenge.graphics;
+
+import javafx.scene.canvas.GraphicsContext;
+import puttingchallenge.model.GameObject;
+
 /**
  * Class that implements the graphical representation of the player.
  */
 public class PlayerGraphicComponent extends AbstractGraphicComponent {
     private static final int HEIGHT = 10;
     private static final int WIDTH = 10;
+
+    private boolean show;
     /**
      * Create a new {@link GraphicComponent} for the player.
      * @param imagePath
@@ -12,6 +18,21 @@ public class PlayerGraphicComponent extends AbstractGraphicComponent {
      */
     public PlayerGraphicComponent(final String imagePath) {
         super(imagePath, WIDTH, HEIGHT);
+        this.show = true;
     }
-
+    /**
+     * Change the player visibility in the game.
+     */
+    public void changeShowStatus() {
+        this.show = !this.show;
+    }
+    /**
+     * Render the player only if visible.
+     */
+    @Override
+    public void draw(final GameObject obj, final GraphicsContext gc) {
+        if (this.show) {
+            super.draw(obj, gc);
+        }
+    }
 }
