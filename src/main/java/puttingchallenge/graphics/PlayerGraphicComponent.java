cf6f0ceda9111d7d826c9513e325c73cecbcac28
Luca Lucioli <luca.lucioli@studio.unibo.it>
2022-06-20 11:32:31 +0200

    docs: fix typo

--- src/main/java/puttingchallenge/graphics/PlayerGraphicComponent.java
+++ src/main/java/puttingchallenge/graphics/PlayerGraphicComponent.java
@@ -1,6 +1,6 @@
 package puttingchallenge.graphics;
 /**
- * Class that implements the graphical representation of a the player.
+ * Class that implements the graphical representation of the player.
  */
 public class PlayerGraphicComponent extends AbstractGraphicComponent {
     private static final int HEIGHT = 10;
