eb143c9ad5e3ed7d24904c695aa4736d8fb1df2f
Lorenzo Colletta <lorenzo.colletta@studio.unibo.it>
2022-06-21 00:10:53 +0200

    Define Colleague interface

---
+++ src/main/java/puttingchallenge/core/Colleague.java
@@ -0,0 +1,21 @@
+package puttingchallenge.core;
+
+import puttingchallenge.model.GameEvent;
+
+/**
+ * Defines an object that needs to interact with other objects (Colleagues), notifying them the occurred event.
+ * A Colleague delegates its interaction with other Colleagues to a Mediator.
+ */
+public interface Colleague {
+
+    /**
+     * @param mediator the mediator to whom to delegate the interaction
+     */
+    void setMediator(Mediator mediator);
+
+    /**
+     * The method to call to notify the Colleague.
+     * @param event to be passed
+     */
+    void notifyEvent(GameEvent<?, ?> event);
+}
