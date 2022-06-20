815448133a4f121a168bb3d9c82de965a4214212
Luca Lucioli <luca.lucioli@studio.unibo.it>
2022-06-21 00:06:06 +0200

    docs: fix typo

--- src/main/java/puttingchallenge/view/GameOverController.java
+++ src/main/java/puttingchallenge/view/GameOverController.java
@@ -6,7 +6,7 @@ import javafx.fxml.FXML;
 /**
  * Class that defines the controller that manages the {@link Scene} shown at the game over.
  * 
-1*/
+ */
 public class GameOverController extends AbstractSceneController {
     /**
      * Method that handle the action on the new game button.
