c71f588d1ee0e64c44a96062126d2a2a72f08cc7
Domenico Francesco Giacobbi <domenico.giacobbi@studio.unibo.it>
2022-06-20 10:46:58 +0200

    feat: update the render method of the LevelController class to print the background image and to manage the refresh of the gameobjects

--- src/main/java/puttingchallenge/view/LevelController.java
+++ src/main/java/puttingchallenge/view/LevelController.java
@@ -1,7 +1,7 @@
 package puttingchallenge.view;
 
+import javafx.scene.image.Image;
 import java.util.List;
-
 import javafx.scene.Scene;
 import javafx.scene.canvas.GraphicsContext;
 import puttingchallenge.model.GameObject;
@@ -12,7 +12,11 @@ import puttingchallenge.model.GameObject;
  */
 public class LevelController extends AbstractSceneController {
 
+    private static final double UPPER_LEFT_X = 0.0;
+    private static final double UPPER_LEFT_Y = 0.0;
+
     private final GraphicsContext gc;
+    private final String background;
 
     /**
      * Build a new {@link LevelController}.
@@ -25,21 +29,33 @@ public class LevelController extends AbstractSceneController {
      *            {@link View} where the {@link Scene} is displayed
      * @param gc
      *            the {@link GraphicsContext} in which the object has to be drawn
-     *
+     * @param background
      */
     public LevelController(final Scene scene,
                            final List<GameObject> gameObjects,
                            final View view,
-                           final GraphicsContext gc) {
+                           final GraphicsContext gc,
+                           final String background) {
         super(scene, gameObjects, view);
         this.gc = gc;
+        this.background = background;
     }
 
     /**
      * {@inheritDoc}
      */
     public void render() {
-       super.getGameObjects().stream().peek(e -> e.draw(gc));
+        final Image backgrounImage = new Image(background);
+        this.gc.drawImage(backgrounImage, 
+                          UPPER_LEFT_X, 
+                          UPPER_LEFT_Y, 
+                          super.getScene().getWidth(), 
+                          super.getScene().getHeight());
+        this.gc.clearRect(UPPER_LEFT_X,
+                          UPPER_LEFT_Y, 
+                          super.getScene().getWidth(), 
+                          super.getScene().getHeight());
+        super.getGameObjects().stream().peek(e -> e.draw(gc));
     }
 
 }
