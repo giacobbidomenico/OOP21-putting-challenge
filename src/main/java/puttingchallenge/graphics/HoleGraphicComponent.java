be66341ee49d836c5f2cda8121ffac3ae69562ce
Domenico Francesco Giacobbi <domenico.giacobbi@studio.unibo.it>
2022-06-21 01:03:39 +0200

    feat: implement the HoleGraphicComponent class

---
+++ src/main/java/puttingchallenge/graphics/HoleGraphicComponent.java
@@ -0,0 +1,24 @@
+package puttingchallenge.graphics;
+
+/**
+ * Class that implements the graphical representation of the game hole.
+ * 
+ */
+public class HoleGraphicComponent extends AbstractGraphicComponent {
+
+    private static final String IMAGE_OF_THE_HOLE = "";
+
+    /**
+     * Build a new {@link HoleGraphicComponent}.
+     * 
+     * @param w
+     *        the width of the {@link Image} where the hole is contained
+     * @param h
+     *        the height of the {@link Image} where the hole is contained
+     */
+    public HoleGraphicComponent(final double w, 
+                                final double h) {
+        super(IMAGE_OF_THE_HOLE, w, h);
+    }
+
+}
