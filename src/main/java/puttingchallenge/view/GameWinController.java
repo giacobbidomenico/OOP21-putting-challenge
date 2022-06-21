a3f950780168a2d2f76e940eebbc1c3938fa8434
GiorgioFantilli <giorgio.fantilli@studio.unibo.it>
2022-06-21 20:30:02 +0200

    fix: GameWonController -> GAmeWinController 	 handleToMenu() -> backToMenu()

--- src/main/java/puttingchallenge/view/GameWinController.java
+++ src/main/java/puttingchallenge/view/GameWinController.java
@@ -6,13 +6,13 @@ import javafx.fxml.FXML;
  * Class that defines the controller that manages the {@link Scene} shown when the user wins the game.
  * 
  */
-public class GameWonController extends AbstractSceneController {
+public class GameWinController extends AbstractSceneController {
     /**
      * Method that handle the action on the menu button.
      * @param event
      */
     @FXML
-    public void handleToMenu(final ActionEvent event) {
+    public void backToMenu(final ActionEvent event) {
         this.getController().notifyEvent(event);
     }
 
