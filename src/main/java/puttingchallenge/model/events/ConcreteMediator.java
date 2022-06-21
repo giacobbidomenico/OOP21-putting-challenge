cc606b359c403e6425d920d84888c9ce0c1cfb79
Lorenzo Colletta <lorenzo.colletta@studio.unibo.it>
2022-06-22 01:04:41 +0200

    improvements: Define a concrete Mediator

--- src/main/java/puttingchallenge/model/events/ConcreteMediator.java
+++ src/main/java/puttingchallenge/model/events/ConcreteMediator.java
@@ -1,34 +1,46 @@
 package puttingchallenge.model.events;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import puttingchallenge.model.GameEvent;
 
 /**
- * 
+ * Implementation of {@link Mediator}.
  */
 public class ConcreteMediator implements Mediator {
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void addColleague(Colleague newColleague) {
+    private final Set<Colleague> colleagues;
 
+    /**
+     * Build a new {@link ConcreteMediator}.
+     */
+    public ConcreteMediator() {
+        this.colleagues = new HashSet<>();
     }
 
     /**
      * {@inheritDoc}
      */
     @Override
-    public void removeColleague(Colleague toRemove) {
-
+    public void addColleague(final Colleague newColleague) {
+        this.colleagues.add(newColleague);
     }
 
     /**
      * {@inheritDoc}
      */
     @Override
-    public void notifyColleagues(GameEvent<?, ?> event, Colleague sender) {
+    public void removeColleague(final Colleague toRemove) {
+        this.colleagues.remove(toRemove);
+    }
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void notifyColleagues(final GameEvent<?, ?> event, final Colleague sender) {
+        this.colleagues.stream().filter(x -> !x.equals(sender)).forEach(x -> x.notifyEvent(event));
     }
 
 }
