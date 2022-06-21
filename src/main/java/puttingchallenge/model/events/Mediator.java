91ecb25759e9cec254ba3ec9953f81f6922dfc17
Lorenzo Colletta <lorenzo.colletta@studio.unibo.it>
2022-06-21 20:19:17 +0200

    improvements: the mediator is now able to remove a colleague and to identify the sender of an event

--- src/main/java/puttingchallenge/model/events/Mediator.java
+++ src/main/java/puttingchallenge/model/events/Mediator.java
@@ -14,8 +14,13 @@ public interface Mediator {
     void addColleague(Colleague newColleague);
 
     /**
-     * @param event 
-     * represents the event occured that the colleagues wants to send
+     * @param toRemove the colleague to be removed
      */
-    void notifyColleagues(GameEvent<?, ?> event);
+    void removeColleague(Colleague toRemove);
+
+    /**
+     * @param event represents the event occured that the colleagues wants to send
+     * @param sender the colleague who wants to send an event
+     */
+    void notifyColleagues(GameEvent<?, ?> event, Colleague sender);
 }
