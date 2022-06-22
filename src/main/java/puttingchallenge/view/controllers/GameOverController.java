91cf2c15b8d660d6042bd8bc4534fb505482dbcf
GiorgioFantilli <giorgio.fantilli@studio.unibo.it>
2022-06-22 18:05:11 +0200

    feat: implement game over scene controller

--- src/main/java/puttingchallenge/view/controllers/GameOverController.java
+++ src/main/java/puttingchallenge/view/controllers/GameOverController.java
@@ -1,25 +1,37 @@
 package puttingchallenge.view.controllers;
 import javafx.event.ActionEvent;
+
 import javafx.fxml.FXML;
+import puttingchallenge.model.events.GameEvent;
+import puttingchallenge.model.events.GameEventImpl;
+import puttingchallenge.model.events.GameEventType;
 /**
  * Class that defines the controller that manages the {@link Scene} shown at the game over.
  * 
  */
 public class GameOverController extends AbstractSceneController {
+
     /**
      * Method that handle the action on the new game button.
-     * @param event
+     * 
+     * @param e
+     *          unused
      */
     @FXML
-    public void handleNewGame(final ActionEvent event) {
-        //TODO
+    public void handleNewGame(final ActionEvent e) {
+        final GameEvent event = new GameEventImpl(GameEventType.START);
+        super.getMediator().notifyColleagues(event, this);
     }
+
     /**
      * Method that handle the action on the menu button.
-     * @param event
+     * 
+     * @param e
+     *          unused
      */
     @FXML
-    public void handleToMenu(final ActionEvent event) {
-        //TODO
+    public void handleToMenu(final ActionEvent e) {
+        final GameEvent event = new GameEventImpl(GameEventType.SHOW_MAIN_MENU);
+        super.getMediator().notifyColleagues(event, this);
     }
 }
