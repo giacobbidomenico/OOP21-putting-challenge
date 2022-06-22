897f7a7088752b30df3b1dddd43c8ff9ae62b9d5
Luca Lucioli <luca.lucioli@studio.unibo.it>
2022-06-22 13:36:50 +0200

    feat: the manager implements Colleague interface

--- src/main/java/puttingchallenge/model/GameStateManager.java
+++ src/main/java/puttingchallenge/model/GameStateManager.java
@@ -1,8 +1,11 @@
 package puttingchallenge.model;
+
+import puttingchallenge.model.events.Colleague;
+
 /**
  * Interface that defines the {@link GameState} manager.
  */
-public interface GameStateManager {
+public interface GameStateManager extends Colleague {
     /**
      * Sets a new current{@link GameState}.
      * @param newState
--- src/main/java/puttingchallenge/model/GameStateManagerImpl.java
+++ src/main/java/puttingchallenge/model/GameStateManagerImpl.java
@@ -1,9 +1,14 @@
 package puttingchallenge.model;
+
+import puttingchallenge.model.events.GameEvent;
+import puttingchallenge.model.events.Mediator;
+
 /**
  * Implementation of {@link GameStateManager} interface.
  */
 public class GameStateManagerImpl implements GameStateManager {
     private GameState currentGameState;
+    private Mediator generalMediator;
     /**
      * {@inheritDoc}
      */
@@ -17,6 +22,20 @@ public class GameStateManagerImpl implements GameStateManager {
     @Override
     public GameState getCurrentState() {
         return this.currentGameState;
+    }
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void setMediator(final Mediator mediator) {
+        this.generalMediator = mediator;
+    }
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void notifyEvent(final GameEvent<?> event) {
+        this.generalMediator.notifyColleagues(event, this);
     }
 
 }
