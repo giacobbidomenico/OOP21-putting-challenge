c7336ec0cc271777b277461537c09938267a21aa
GiorgioFantilli <giorgio.fantilli@studio.unibo.it>
2022-06-22 18:02:22 +0200

    feat: implement game win scene controller

--- src/main/java/puttingchallenge/view/controllers/GameWinController.java
+++ src/main/java/puttingchallenge/view/controllers/GameWinController.java
@@ -2,18 +2,26 @@ package puttingchallenge.view.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import puttingchallenge.model.events.GameEvent;
+import puttingchallenge.model.events.GameEventImpl;
+import puttingchallenge.model.events.GameEventType;
+
 /**
  * Class that defines the controller that manages the {@link Scene} shown when the user wins the game.
  * 
  */
 public class GameWinController extends AbstractSceneController {
+
     /**
      * Method that handle the action on the menu button.
-     * @param event
+     * 
+     * @param e
+     *          unused
      */
     @FXML
-    public void backToMenu(final ActionEvent event) {
-        
+    public void backToMenu(final ActionEvent e) {
+        final GameEvent event = new GameEventImpl(GameEventType.SHOW_MAIN_MENU);
+        super.getMediator().notifyColleagues(event, this);
     }
 
 }
