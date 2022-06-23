207c33f7070b8d2ae6559d5aed3c38d64e1f6844
Lorenzo Colletta <lorenzo.colletta@studio.unibo.it>
2022-06-23 13:33:28 +0200

    improvements: add gameEngineImpl events management

--- src/main/java/puttingchallenge/core/GameLoopImpl.java
+++ src/main/java/puttingchallenge/core/GameLoopImpl.java
@@ -1,13 +1,23 @@
 package puttingchallenge.core;
 
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 import javafx.stage.Stage;
+import javafx.util.Pair;
 import puttingchallenge.model.GameStateManager;
 import puttingchallenge.model.GameStateManagerImpl;
 import puttingchallenge.model.events.Colleague;
 import puttingchallenge.model.events.ConcreteMediator;
 import puttingchallenge.model.events.GameEvent;
+import puttingchallenge.model.events.GameEventImpl;
+import puttingchallenge.model.events.GameEventType;
+import puttingchallenge.model.events.GameEventWithDetailsImpl;
 import puttingchallenge.model.events.Mediator;
+import puttingchallenge.model.gameobjects.GameObject;
+import puttingchallenge.view.SceneType;
 import puttingchallenge.view.View;
 import puttingchallenge.view.ViewImpl;
 
@@ -20,6 +30,8 @@ public class GameLoopImpl implements GameEngine, Colleague {
     private Mediator mediator;
     private final GameStateManager gameState;
     private final View view;
+    private final Queue<GameEvent> receivedEvents;
+    private boolean isOver;
 
     /**
      * Builds a {@link GameLoopImpl}.
@@ -29,6 +41,8 @@ public class GameLoopImpl implements GameEngine, Colleague {
         final Mediator mediator = new ConcreteMediator();
         this.gameState = new GameStateManagerImpl();
         this.view = new ViewImpl(primaryStage);
+        this.receivedEvents = new LinkedList<>();
+        this.isOver = false;
 
         mediator.addColleague(gameState);
         mediator.addColleague(view);
@@ -40,7 +54,7 @@ public class GameLoopImpl implements GameEngine, Colleague {
     }
 
     private void waitCycleTime(final long startTime) {
-        long delta = System.currentTimeMillis() - startTime;
+        final long delta = System.currentTimeMillis() - startTime;
         if (delta < FRAME_TIME) {
             try {
                 Thread.sleep(FRAME_TIME - delta);
@@ -56,16 +70,50 @@ public class GameLoopImpl implements GameEngine, Colleague {
         view.render();
     }
 
+    private void processInput() {
+        while (this.receivedEvents.size() > 0) {
+            final GameEvent event = receivedEvents.poll();
+            switch (event.getEventType()) {
+                case QUIT:
+                    this.isOver = true;
+                    break;
+                case WIN:
+                    final Pair<SceneType, List<GameObject>> winDetail = new Pair<>(SceneType.GAME_WIN, this.gameState.getCurrentEnvironment().getObjects());
+                    final GameEvent winEvent = new GameEventWithDetailsImpl<>(GameEventType.WIN, winDetail);
+                    this.mediator.notifyColleagues(winEvent, this);
+                    break;
+                case GAMEOVER:
+                    final Pair<SceneType, List<GameObject>> gameOverDetail = new Pair<>(SceneType.GAME_OVER, this.gameState.getCurrentEnvironment().getObjects());
+                    final GameEvent gameOverEvent = new GameEventWithDetailsImpl<>(GameEventType.GAMEOVER, gameOverDetail);
+                    this.mediator.notifyColleagues(gameOverEvent, this);
+                    break;
+                case SHOW_LEADERBOARD:
+                    final Pair<SceneType, List<GameObject>> leaderboardDetail = new Pair<>(SceneType.LEADEARBOARD, this.gameState.getCurrentEnvironment().getObjects());
+                    final GameEvent leaderboardEvent = new GameEventWithDetailsImpl<>(GameEventType.SHOW_LEADERBOARD, leaderboardDetail);
+                    this.mediator.notifyColleagues(leaderboardEvent, this);
+                    break;
+                case SHOW_MAIN_MENU:
+                    final Pair<SceneType, List<GameObject>> mainMenuDetail = new Pair<>(SceneType.MAIN_MENU, this.gameState.getCurrentEnvironment().getObjects());
+                    final GameEvent mainMenuEvent = new GameEventWithDetailsImpl<>(GameEventType.SHOW_MAIN_MENU, mainMenuDetail);
+                    this.mediator.notifyColleagues(mainMenuEvent, this);
+                    break;
+                default: 
+                    break;
+            }
+        }
+    }
+
     /**
      * {@inheritDoc}
      */
     @Override
     public void gameLoop() {
         long previusCycleStartTime = System.currentTimeMillis();
-        while (true) {
+        while (!this.isOver) {
             final long currentCycleStartTime = System.currentTimeMillis();
             final long delta = currentCycleStartTime - previusCycleStartTime;
 
+            this.processInput();
             this.updateGame(delta);
             this.render();
 
@@ -87,7 +135,7 @@ public class GameLoopImpl implements GameEngine, Colleague {
      */
     @Override
     public void notifyEvent(final GameEvent event) {
-        // TODO Auto-generated method stub
+        this.receivedEvents.add(event);
     }
 
 }
