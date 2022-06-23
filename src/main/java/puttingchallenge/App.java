50b3074991c7e5ee70456179df374f6623df46ad
Domenico Francesco Giacobbi <domenico.giacobbi@studio.unibo.it>
2022-06-23 23:46:27 +0200

    fix: fix the loading of the application by inserting an external class

--- src/main/java/puttingchallenge/App.java
+++ src/main/java/puttingchallenge/App.java
@@ -1,31 +1,21 @@
 package puttingchallenge;
 
 import javafx.application.Application;
-import javafx.stage.Stage;
-import puttingchallenge.core.GameEngine;
-import puttingchallenge.core.GameLoopImpl;
 
 /**
  * The class that start the application.
  */
-public final class App extends Application {
-
-    /**
-     * The entry point of the application.
-     * 
-     * @param args
-     *          unused
-     */
-    public static void main(final String[] args) {
-        launch(args);
+public final class App {
+    private App() {
     }
 
     /**
-     * {@inheritDoc}
+     * Main class.
+     * 
+     * @param args
+     *          not used
      */
-    @Override
-    public void start(final Stage primaryStage) throws Exception {
-        final GameEngine gameEngine = new GameLoopImpl(primaryStage);
-        gameEngine.launch();
+    public static void main(final String... args) {
+        Application.launch(ApplicationLoad.class, args);
     }
 }
