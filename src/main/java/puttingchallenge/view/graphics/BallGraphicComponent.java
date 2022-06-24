80469bf68531bddef92036bd9a070bb9878cd0cb
Luca Lucioli <luca.lucioli@studio.unibo.it>
2022-06-24 22:54:49 +0200

    fix merge

--- src/main/java/puttingchallenge/view/graphics/BallGraphicComponent.java
+++ src/main/java/puttingchallenge/view/graphics/BallGraphicComponent.java
@@ -8,13 +8,7 @@ import java.io.File;
 public final class BallGraphicComponent extends AbstractGraphicComponent {
 
     private static final String SEP = File.separator;
-<<<<<<< HEAD
     private static final String SKIN_PATH = SEP + "skins" + SEP + "ball.png";
-=======
-    private static final String SKIN_PATH = SEP + "resources"
-                                            + SEP + "skins"
-                                            + SEP + "ball.png";
->>>>>>> 2f8b3b7bcb87180a838762cd99894fd6639b4e98
 
     /**
      * Build a new {@link GraphicComponent} for the game ball.
