c50334ff7c20b2da9fc3a2b15ad45eb15cee53ab
Lorenzo Colletta <lorenzo.colletta@studio.unibo.it>
2022-06-25 17:12:00 +0200

    feat: ActiveBoundingBox can now resolve a normal to its self

--- src/main/java/puttingchallenge/model/collisions/ActiveBoundingBox.java
+++ src/main/java/puttingchallenge/model/collisions/ActiveBoundingBox.java
@@ -1,5 +1,8 @@
 package puttingchallenge.model.collisions;
 
+import puttingchallenge.common.Point2D;
+import puttingchallenge.common.Vector2D;
+
 /**
  * Represents a bounding box which verify whether it collides with a passive bounding box
  * hence "active".
@@ -12,5 +15,11 @@ public interface ActiveBoundingBox {
      * @return whether this boounding box and circle collide
      */
     boolean isColliding(PassiveCircleBoundingBox circle);
+
+    /**
+     * @param pointOnActiveBoundingBox
+     * @return the normal vector to the ActiveBoundingBox at the point passed
+     */
+    Vector2D getNormal(Point2D pointOnActiveBoundingBox);
 
 }
