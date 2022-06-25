2c4da05364fb059a9ddcbf5ff2b9a41b7a66316e
Luca Lucioli <luca.lucioli@studio.unibo.it>
2022-06-25 13:04:55 +0200

    fix: add FXML import

--- src/main/java/puttingchallenge/view/controllers/LevelController.java
+++ src/main/java/puttingchallenge/view/controllers/LevelController.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
