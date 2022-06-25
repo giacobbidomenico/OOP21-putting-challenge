1424b4e9d0d2444b49dc1f764946f4dbf4d7c53d
Lorenzo Colletta <lorenzo.colletta@studio.unibo.it>
2022-06-25 11:13:37 +0200

    feat: introduce a class implementing DynamicBoundingBox.CollisionTest

--- src/main/java/puttingchallenge/model/collisions/ConcreteDynamicBoundingBox.java
+++ src/main/java/puttingchallenge/model/collisions/ConcreteDynamicBoundingBox.java
@@ -1,5 +1,10 @@
 package puttingchallenge.model.collisions;
 
+import java.util.Optional;
+
+import puttingchallenge.common.Point2D;
+import puttingchallenge.common.Vector2D;
+
 /**
  * It wraps an ActiveBoundingBox so that its collision can be tested to a PassiveCircleBoundingBox dynamically.
  */
@@ -23,5 +28,55 @@ public class ConcreteDynamicBoundingBox implements DynamicBoundingBox {
         return null;
     }
 
+    /**
+     * Represents a concrete collision test between a {@link PassiveCircleBoundingBox} and a {@link ActiveBoundingBox}.
+     */
+    public class ConcreteCollisionTest implements CollisionTest {
 
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public boolean isCollisionOccurred() {
+            // TODO Auto-generated method stub
+            return false;
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public ActiveBoundingBox getActiveBoundingBox() {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public Optional<Point2D> getEstimatedPointOfImpact() {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public Optional<Vector2D> getActiveBBSideNormal() {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public Optional<Point2D> getPassiveBoxPositionBeforeCollisions() {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+    }
 }
