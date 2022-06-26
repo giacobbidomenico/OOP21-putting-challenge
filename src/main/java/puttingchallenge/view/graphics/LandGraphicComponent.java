d63779f8bcda2a924dd2a63d0f6f254317253a58
Domenico Francesco Giacobbi <domenico.giacobbi@studio.unibo.it>
2022-06-26 18:49:20 +0200

    fix: fix merge problems

--- src/main/java/puttingchallenge/view/graphics/LandGraphicComponent.java
+++ src/main/java/puttingchallenge/view/graphics/LandGraphicComponent.java
@@ -3,11 +3,7 @@ package puttingchallenge.view.graphics;
 import java.io.File;
 
 /**
-<<<<<<< HEAD
  * Class that implements the graphical representation of the land in the game.
-=======
- * Class that implements the graphical representation of a wall in the game.
->>>>>>> master
  * 
  */
 public class LandGraphicComponent extends AbstractGraphicComponent {
@@ -23,14 +19,6 @@ public class LandGraphicComponent extends AbstractGraphicComponent {
      *         the width of the {@link Image} where the land is contained
      * @param h
      *         the height of the {@link Image} where the land is contained
-=======
-     * Build a new {@link GraphicComponent} for a wall in the game.
-     * 
-     * @param w
-     *         the width of the {@link Image} where the wall is contained
-     * @param h
-     *         the height of the {@link Image} where the wall is contained
->>>>>>> master
      */
     public LandGraphicComponent(final double w, final double h) {
         super(TEXTURE_PATH_IMAGE, w, h);
