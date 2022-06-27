14d7c4c4f67755b3e8cd1564902d73e113978aef
Lorenzo Colletta <lorenzo.colletta@studio.unibo.it>
2022-06-27 16:20:57 +0200

    feat: implementing the table

--- src/main/java/puttingchallenge/view/controllers/LeaderboardController.java
+++ src/main/java/puttingchallenge/view/controllers/LeaderboardController.java
@@ -1,8 +1,18 @@
 package puttingchallenge.view.controllers;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import puttingchallenge.core.FileManager;
 import puttingchallenge.model.events.GameEvent;
 import puttingchallenge.model.events.GameEventImpl;
 import puttingchallenge.model.events.GameEventType;
@@ -13,10 +23,33 @@ import puttingchallenge.model.events.GameEventType;
  */
 public class LeaderboardController extends AbstractSceneController {
 
-    @FXML private TableView<String> table;
+    @FXML private TableView<Score> table;
+    @FXML private TableColumn<Score, Integer> attempts;
+    @FXML private TableColumn<Score, String> scores;
 
+    /**
+     * Builds {@link LeaderboardController} and initializes the table.
+     */
     public LeaderboardController() {
-        
+
+        this.attempts.setCellValueFactory(new PropertyValueFactory<Score, Integer>("Attempt"));
+        this.scores.setCellValueFactory(new PropertyValueFactory<Score, String>("Score"));
+
+        final List<Score> scores = new ArrayList<>();
+        try (BufferedReader f = new BufferedReader(
+                new InputStreamReader(
+                        new FileInputStream(FileManager.LEADERBOARD_FILE)))) {
+            String line;
+            int counter = 1;
+            while ((line = f.readLine()) != null) {
+                scores.add(new Score(line, counter));
+                counter++;
+            }
+            this.table.setItems(null);
+        } catch (IOException e) {
+            scores.clear();
+        }
+        table.getItems().setAll(scores);
     }
 
     /**
@@ -26,5 +59,36 @@ public class LeaderboardController extends AbstractSceneController {
     public void quitGame() {
         final GameEvent event = new GameEventImpl(GameEventType.QUIT);
         super.getMediator().notifyColleagues(event, this);
+    }
+
+    /**
+     * Simple class that models the score.
+     */
+    private class Score {
+        private final String scoreValue;
+        private final Integer number;
+
+        /**
+         * @param score
+         * @param number position in the table
+         */
+        Score(final String score, final Integer number) {
+            this.scoreValue = score;
+            this.number = number;
+        }
+
+        /**
+         * @return score
+         */
+        public String getScore() {
+            return this.scoreValue;
+        }
+
+        /**
+         * @return position in the table
+         */
+        public Integer getNumber() {
+            return this.number;
+        }
     }
 }
