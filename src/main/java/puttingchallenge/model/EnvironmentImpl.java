8fe2c7b7d18a5f9d5501166afad2759268613c6a
Domenico Francesco Giacobbi <domenico.giacobbi@studio.unibo.it>
2022-06-27 23:29:18 +0200

    fix: fix the movement of the player

--- src/main/java/puttingchallenge/model/EnvironmentImpl.java
+++ src/main/java/puttingchallenge/model/EnvironmentImpl.java
@@ -130,25 +130,6 @@ public class EnvironmentImpl implements Environment {
      */
     @Override
     public void movePlayer() {
-        /*
-        if (!this.isBallStationary()) {
-            throw new IllegalStateException();
-        }
-        System.out.println("moved");
-        this.notidiedBallStoped = false;
-        final var calcDist = new Point2D(this.container.getWidth() *  (PERC_DISTANCE / 100),
-                                         this.container.getHeight() * (PERC_DISTANCE / 100));
-        final var pos = this.ball.getPosition();
-        if ((pos.getX() - calcDist.getX()) >= 0) {
-            this.player.setFlip(false);
-            this.player.setPosition(new Point2D(pos.getX() - calcDist.getX(), pos.getY()));
-            return;
-        }
-        if ((pos.getX() + calcDist.getX()) < this.container.getWidth()) {
-            this.player.setFlip(true);
-            this.player.setPosition(new Point2D(pos.getX() + calcDist.getX(), pos.getY()));
-            return;
-        }*/
         if (!this.isBallStationary()) {
             throw new IllegalStateException();
         }
