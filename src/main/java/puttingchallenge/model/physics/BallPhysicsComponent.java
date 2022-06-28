b5ae874498894e643a1a4628a49c2b2291676a47
GiorgioFantilli <giorgio.fantilli@studio.unibo.it>
2022-06-28 18:55:24 +0200

    fix: check collision do not need the time elapsed

--- src/main/java/puttingchallenge/model/physics/BallPhysicsComponent.java
+++ src/main/java/puttingchallenge/model/physics/BallPhysicsComponent.java
@@ -48,7 +48,7 @@ public class BallPhysicsComponent extends AbstractPhysicsComponent {
             final BallPhysicsComponent clone = new BallPhysicsComponent(radius);
             clone.setVelocity(new Vector2D(this.getVelocity()));
 
-            final Optional<CollisionTest> infoOpt = env.checkCollisions(((BallObjectImpl) obj).getHitBox(), clone, obj.getPosition(), dt);
+            final Optional<CollisionTest> infoOpt = env.checkCollisions(((BallObjectImpl) obj).getHitBox(), clone, obj.getPosition());
             Point2D nextPos;
             if (infoOpt.isPresent()) {
                 final CollisionTest info = infoOpt.get();
