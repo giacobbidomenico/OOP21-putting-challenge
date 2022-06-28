996c9703357dd6d2bd69816cb356520042727ca0
Domenico Francesco Giacobbi <domenico.giacobbi@studio.unibo.it>
2022-06-28 13:11:09 +0200

    feat: add equals method to the PhysicsComponents

--- src/main/java/puttingchallenge/model/physics/AbstractPhysicsComponent.java
+++ src/main/java/puttingchallenge/model/physics/AbstractPhysicsComponent.java
@@ -1,5 +1,7 @@
 package puttingchallenge.model.physics;
 
+import java.util.Objects;
+
 import puttingchallenge.common.Vector2D;
 import puttingchallenge.model.Environment;
 import puttingchallenge.model.gameobjects.GameObject;
@@ -28,6 +30,32 @@ public abstract class AbstractPhysicsComponent implements PhysicsComponent {
      */
     public Vector2D getVelocity() {
         return this.vel;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(vel);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (obj instanceof PhysicsComponent) {
+            final PhysicsComponent phys = (PhysicsComponent) obj;
+            return phys.getVelocity().equals(this.vel);
+        }
+        return false;
     }
 
 }
