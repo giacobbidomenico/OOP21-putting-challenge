c58ca44513a1723200dc14f8c86cb80dcf1bb7a0
GiorgioFantilli <giorgio.fantilli@studio.unibo.it>
2022-06-30 00:10:48 +0200

    fix: removed useless import

--- src/main/java/puttingchallenge/view/SceneLoader.java
+++ src/main/java/puttingchallenge/view/SceneLoader.java
@@ -11,21 +11,9 @@ import org.apache.commons.io.IOUtils;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundImage;
-import javafx.scene.layout.BackgroundPosition;
-import javafx.scene.layout.BackgroundRepeat;
-import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.VBox;
 import puttingchallenge.common.FileManager;
 import puttingchallenge.model.gameobjects.GameObject;
-import puttingchallenge.view.controllers.LevelController;
 import puttingchallenge.view.controllers.SceneController;
 
 /**
